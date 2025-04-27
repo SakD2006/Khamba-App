8052edf870edd6fac90038185e7ea59b8bc5d18a
Saksham <dubey.saksham@icloud.com>
2025-04-28 05:27:22 +0530

    initial commit

---
+++ Khamba/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Khamba
+//
+//  Created by Saksham Dubey on 28/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Khamba/KhambaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KhambaApp.swift
+//  Khamba
+//
+//  Created by Saksham Dubey on 28/04/25.
+//
+
+import SwiftUI
+
+@main
+struct KhambaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
